edf99ca253a2e34f5263afcdcc36305d2524cb50
Adoah <xadoahx@gmail.com>
2021-04-17 17:20:48 -0400

    pushing to bitbucket

--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Spinning reverse proxy and database down"
-echo "If you're still working on sites locally, they will no longer be 'hosted'  on your local machine"
+echo "If you're still working on sites locally, they will no longer be 'hosted' on your local machine"
 
 docker-compose down
